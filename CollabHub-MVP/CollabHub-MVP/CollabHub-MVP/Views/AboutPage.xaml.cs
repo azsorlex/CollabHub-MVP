1c2c48aedf312c590c8eaa5943cfbb6aba9620cb
Alexander Rozsa <arozsa@protonmail.com>
2020-09-15 16:34:22 +1000

    Created project
    Used the Flyout template, updated packages, installed Refractored.MvvmHelpers, and tested deployment to my phone.

---
+++ CollabHub-MVP/CollabHub-MVP/CollabHub-MVP/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CollabHub_MVP.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CollabHub-MVP/CollabHub-MVP/CollabHub-MVP/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CollabHub_MVP.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CollabHub_MVP.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+    }
+}

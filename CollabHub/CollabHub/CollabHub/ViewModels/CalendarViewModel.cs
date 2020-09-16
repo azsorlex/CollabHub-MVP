46253579364a39f7cb7f1b28191cd2c5fc137319
Noah Hartigan <n10463879@qut.edu.au>
2020-09-16 16:44:38 +1000

    Modified calendar pages

---
+++ CollabHub/CollabHub/CollabHub/ViewModels/CalendarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using MvvmHelpers.Commands;
+using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CollabHub.Views;
+
+namespace CollabHub.ViewModels
+{
+    class CalendarViewModel : BaseViewModel
+    {
+    }
+}

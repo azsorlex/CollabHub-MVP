cc090482b528cf210b162a0a37a582d45a93d5e9
VanillaHoward <rhow97@gmail.com>
2020-09-23 17:29:16 +1000

    Added DataStore for and User Model for storing dummy data for chat screen. Added ListView to Chat screen, populated with dummy data

---
+++ CollabHub/CollabHub/CollabHub/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollabHub.Models
+{
+    class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Name { get { return $"{FirstName} {LastName}"; } }
+
+        public string Greeting
+        {
+            get { return $"Hi! May name is {Name}. What's yours?"; }
+        }
+
+        public string Initials
+        {
+            get
+            {
+                string firstInitial = this.FirstName[0].ToString();
+                string secondInitial = this.LastName[0].ToString();
+                string initials = firstInitial += secondInitial;
+
+                return initials.ToUpper();
+            }
+        }
+    }
+}

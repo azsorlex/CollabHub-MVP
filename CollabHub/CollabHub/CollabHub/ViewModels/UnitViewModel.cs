ed49db3002f580570e9446e9adb8c3c3d1675c95
peter <superpoku12@gmail.com>
2020-10-06 19:41:44 +1000

    Trouble with UnitPage Binding

--- CollabHub/CollabHub/CollabHub/ViewModels/UnitViewModel.cs
+++ CollabHub/CollabHub/CollabHub/ViewModels/UnitViewModel.cs
@@ -1,14 +1,37 @@
 ﻿using Xamarin.Forms;
 using MvvmHelpers.Commands;
 using MvvmHelpers;
-using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System;
 using System.Text;
 using CollabHub.Views;
+using CollabHub.Models;
+using CollabHub.Services;
 
 namespace CollabHub.ViewModels
 {
-    class UnitViewModel :BaseViewModel
+    class UnitViewModel : BaseViewModel
     {
+
+        //private ObservableCollection<SemesterInfo> semesterInfos;
+
+        public ObservableCollection<SemesterInfo> SemesterInfos { get; set; }
+
+        public UnitViewModel()
+        {
+            SetUpInfo();
+
+        }
+
+        void SetUpInfo()
+        {
+            SemesterInfos = new ObservableCollection<SemesterInfo>()
+            {
+                new SemesterInfo { SemesterTrack = "Semester 2 2020 (Current)"},
+                new SemesterInfo { SemesterTrack = "Semester 1 2020"},
+                new SemesterInfo { SemesterTrack = "Semester 2 2019"}
+            };
+        }
     }
 }

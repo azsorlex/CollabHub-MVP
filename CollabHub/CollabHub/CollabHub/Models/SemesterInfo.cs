bad59e65059d5ae1563047be77b68114c224528e
peter <superpoku12@gmail.com>
2020-10-07 06:47:19 +1000

    Updated UnitPage and added more mockdata to populate Units Screens

--- CollabHub/CollabHub/CollabHub/Models/SemesterInfo.cs
+++ CollabHub/CollabHub/CollabHub/Models/SemesterInfo.cs
@@ -6,7 +6,13 @@ namespace CollabHub.Models
 {
     public class SemesterInfo
     {
-        //public string UnitCode { get; set; }
+        public string FirstUnitCode { get; set; }
+
+        public string SecondUnitCode { get; set; }
+
+        public string ThirdUnitCode { get; set; }
+
+        public string FourthUnitCode { get; set; }
 
         public string SemesterTrack { get; set; }
     }
--- CollabHub/CollabHub/CollabHub/Services/SemesterDataStore.cs
+++ CollabHub/CollabHub/CollabHub/Services/SemesterDataStore.cs
@@ -11,15 +11,25 @@ namespace CollabHub.Services
         {
             new SemesterInfo()
             {
-                SemesterTrack = "Semester 2 2020 (Current)"
+                SemesterTrack = "Semester 2 2020 (Current)",
+                FirstUnitCode = "IFB398",
+                SecondUnitCode = "IAB330",
+                ThirdUnitCode = "CAB303"
             },
             new SemesterInfo()
             {
-                SemesterTrack = "Semester 1 2020"
+                SemesterTrack = "Semester 1 2020",
+                FirstUnitCode = "CAB203",
+                SecondUnitCode= "CAB302",
+                ThirdUnitCode = "IFB295"
             },
             new SemesterInfo()
             {
-                SemesterTrack = "Previous Semesters"
+                SemesterTrack = "Previous Semesters",
+                FirstUnitCode = "CAB201",
+                SecondUnitCode = "CAB202",
+                ThirdUnitCode = "CAB230",
+                FourthUnitCode = "EGB100"
             }
         };
     }

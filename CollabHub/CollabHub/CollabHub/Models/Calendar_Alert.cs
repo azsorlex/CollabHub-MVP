c0ff35883d1a3ccd6a99fa5f23315c7cd2f1ccdc
Noah Hartigan <n10463879@qut.edu.au>
2020-10-07 16:48:07 +1000

    Updated add alert page.

--- CollabHub/CollabHub/CollabHub/Models/Calendar_Alert.cs
+++ CollabHub/CollabHub/CollabHub/Models/Calendar_Alert.cs
@@ -11,12 +11,16 @@ namespace CollabHub.Models
         public string Date { get; set; }
         public string Time { get; set; }
         public string Icon { get; set; }
+        public string Frequency { get; set; }
+        public string Subject { get; set; }
 
-        public Calendar_Alert(string Name, string Date, string Time)
+        public Calendar_Alert(string Name, string Date, string Time, string Frequency, string Subject)
         {
             this.Name = Name;
             this.Date = Date;
             this.Time = Time;
+            this.Time = Frequency;
+            this.Time = Subject;
             this.Icon = "calendar.png";
 
         }

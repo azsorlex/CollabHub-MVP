dc47ad0a46276c1c3801b9c1868f00e3cc273861
VanillaHoward <rhow97@gmail.com>
2020-10-14 13:31:22 +1000

    Added further params for UserChat page navigation. Finalised SQL implementation. Tests of CRUD commands to database to follow.

---
+++ CollabHub/CollabHub/CollabHub/Models/Chat/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollabHub.Models.Chat
+{
+    public class Message
+    {
+        public string Text { get; set; }
+
+        public string Timestamp { get; set; }
+    }
+}

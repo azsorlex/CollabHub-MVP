9a61f24cbc83d822920416499c596a8e89db5c80
peter <superpoku12@gmail.com>
2020-10-18 19:04:29 +1000

    UnitsPage Improvements

--- CollabHub/CollabHub/CollabHub/Models/SemesterInfo.cs
+++ CollabHub/CollabHub/CollabHub/Models/SemesterInfo.cs
@@ -6,20 +6,9 @@ namespace CollabHub.Models
 {
     public class SemesterInfo
     {
-        public string FirstUnitCode { get; set; }
-
-        public string SecondUnitCode { get; set; }
-
-        public string ThirdUnitCode { get; set; }
-
-        public string FourthUnitCode { get; set; }
-
         public string SemesterTrack { get; set; }
-
-        public string ResourceHeading1 { get; set; }
-        public string ResourceHeading2 { get; set; }
-        public string ResourceHeading3 { get; set; }
-
+        public List<String> UnitContentHeading { get; set; }
+        public List<String> UnitCode { get; set; }
         public List<String> Unit1UnitInfo { get; set; }
         public List<String> Unit1LearningMat {get; set;}
         public List<String> Unit1AssessmentT { get; set; }
